ae0f8728d405eada6f0dfef9b1249bf0cf41b245
fretbuzz <JosephSeverini2017@u.northwestern.edu>
2017-05-07 17:34:42 -0500

    Can add to wishlist and fixed backend connection

--- src/pages/wish-list/wish-list.ts
+++ src/pages/wish-list/wish-list.ts
@@ -20,4 +20,41 @@ export class WishListPage {
     this.angFireDB = angFire;
     this.wishList = angFire.database.list('/wishlist');
   }
+
+  addToWishlist():void{
+  	let prompt = this.alertCtrl.create({
+  		title: 'New Task',
+  		message: 'Enter the task details in the form below',
+  		inputs: [
+  			{
+  				name: 't_name',
+  				placeholder: "Task Name"
+  			},
+  			{
+  				name: 't_description',
+  				placeholder: "Task Description"
+  			},
+  		],
+  		buttons: [
+  			{
+  				text: "Cancel",
+  				handler: data => {
+  					console.log('cancel clicked')
+  				}
+  			},
+  			{
+  				text: "Submit",
+  				handler: data => {
+  					var newRef = this.wishList.push({
+  						p_name: data.t_name,
+              p_description: data.t_description,
+  					})
+  				}
+  			}
+  		]
+  	});
+
+  	prompt.present();
+  }
+
 }
