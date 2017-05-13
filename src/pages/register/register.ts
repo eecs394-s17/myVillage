c411a86dca4cdf1920e9ca0ca84cb56fd0e43d7e
fretbuzz <JosephSeverini2017@u.northwestern.edu>
2017-05-13 15:22:04 -0500

    Registration working

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Auth, User, UserDetails, IDetailedError } from '@ionic/cloud-angular';
+import { LoginPage } from "../login/login";
 
 /*
   Generated class for the Register page.
@@ -12,11 +14,31 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'register.html'
 })
 export class RegisterPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
-
+  details: UserDetails = {'email': '', 'password': ''};
+  lastname: any;
+  firstname: any;
+  email: any;
+    
+  constructor(private nav: NavController, private ionicAuth: Auth, public user: User)
+  { } 
+  
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
+  }
+
+  public register() {
+    this.ionicAuth.signup(this.details).then(() => {
+      console.log(this.details.email + this.details.password);
+      this.nav.setRoot(LoginPage);
+    }, (err: IDetailedError<string[]>) => {
+      for (let e of err.details) {
+        if (e === 'conflict_email') {
+          alert('Email already exists.');
+        } else {
+          alert(e);
+        }
+      }
+    });
   }
 
 }
